808ac7879bd7af6b6605cdda566e5fb2236a9858
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2017-11-09 14:10:11 +0100

    The book is parcellable

--- app/src/main/java/fr/android/androidexercises/Book.java
+++ app/src/main/java/fr/android/androidexercises/Book.java
@@ -1,8 +1,9 @@
 package fr.android.androidexercises;
 
-// TODO use parcel
+import android.os.Parcel;
+import android.os.Parcelable;
 
-class Book {
+class Book implements Parcelable {
 
     private String name;
     private String author;
@@ -12,11 +13,39 @@ class Book {
         this.author = author;
     }
 
+    protected Book(Parcel in) {
+        name = in.readString();
+        author = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
     public String getName() {
         return name;
     }
 
     String getAuthor() {
         return author;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(author);
     }
 }
