218cbda9219a53f21b9d508a0a2ea4c0234a8634
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2017-11-10 17:20:03 +0100

    Try async service

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -1,12 +1,16 @@
 package fr.android.androidexercises
 
+import android.app.IntentService
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class LibraryActivity : AppCompatActivity(), Step0Fragment.OnNextStep0Listener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        startService(Intent(this, JobService::class.java))
         setContentView(R.layout.activity_library)
         supportFragmentManager.beginTransaction()
                 .replace(R.id.containerFrameLayout, Step0Fragment(), Step0Fragment::class.java.simpleName)
@@ -19,5 +23,17 @@ class LibraryActivity : AppCompatActivity(), Step0Fragment.OnNextStep0Listener {
                 .addToBackStack("coucou")
                 .commit()
     }
+
+//    class JobService: IntentService("coucou") {
+//        override fun onHandleIntent(intent: Intent?) {
+//            val endTime = System.currentTimeMillis() + 5 * 1000
+//            try {
+//                java.lang.Object().wait(endTime - System.currentTimeMillis())
+//            } catch (ignored: Exception) {}
+//
+//            Toast.makeText(this, "It's finish", Toast.LENGTH_LONG).show();
+//        }
+//
+//    }
 
 }
