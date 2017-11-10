ec5295fad525c8aabc9b3a4d768357a4d6336244
Anaël Chardan [aka Nanou] <anael.chardan@gmail.com>
2017-11-10 15:04:51 +0100

    Work with last version of robolectric

--- app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
+++ app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
@@ -1,5 +1,36 @@
 package fr.android.androidexercises;
 
+import org.assertj.android.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
 public class JUnitLoginActivityTest {
+    LoginActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.setupActivity(LoginActivity.class);
+    }
+
+    @Test
+    public void check_init() throws Exception {
+        activity.notLogged();
+
+        Assertions.assertThat(activity.loginLayout).isVisible();
+        Assertions.assertThat(activity.loggedText).isGone();
+
+    }
+
+    @Test
+    public void should_set_logged_state() throws Exception {
+        activity.logged();
+
+        Assertions.assertThat(activity.loginLayout).isGone();
+        Assertions.assertThat(activity.loggedText).isVisible();
+    }
 
 }
--- app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
+++ app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
@@ -1,5 +1,50 @@
 package fr.android.androidexercises;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterTest {
+    @Mock LoginActivity activity;
+    @InjectMocks LoginPresenter loginPresenter;
+
+    @Test
+    public void password_should_be_valid() throws Exception {
+        loginPresenter.checkCredentials("password");
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
+
+    @Test
+    public void password_is_not_valid_is_empty() throws Exception {
+        loginPresenter.checkCredentials("");
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
+
+    @Test
+    public void password_is_not_valid_is_shorter_than_3() throws Exception {
+        loginPresenter.checkCredentials("ab");
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
+
+    @Test
+    public void password_should_be_valid_with_3() throws Exception {
+        loginPresenter.checkCredentials("pas");
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
+
+    @Test
+    public void password_should_be_invalid_with_null() throws Exception {
+        loginPresenter.checkCredentials(null);
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
 
 }
